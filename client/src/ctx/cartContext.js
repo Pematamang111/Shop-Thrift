57bcafe0ecc116f287e9d5dfda3eb2ccd6c49403
Pema Sherpa <pematamang111@gmail.com>
2024-07-03 12:24:27 -0400

    add addProduct in cartProvider

--- client/src/ctx/cartContext.js
+++ client/src/ctx/cartContext.js
@@ -2,15 +2,29 @@ import { useContext, createContext, useState } from 'react';
 
 const cartContext = createContext();
 
-export const cartProvider = ({children}) => {
-const [products, setProducts] = useState([]);
-const [isOpen, setIsOpen] = useState(false);
+export const cartProvider = ({ children }) => {
+	const [products, setProducts] = useState([]);
+	const [isOpen, setIsOpen] = useState(false);
 
-const toggleCart = () => setIsOpen(prev => !prev)
-}
+	const toggleCart = () => setIsOpen((prev) => !prev);
 
+	const addProduct = (product) => {
+		const isExisting = product.find((p) => p.id === product.id);
 
+		if (isExisting) {
+			setProducts((prev) => {
+				const updatedProducts = prev.map((p) => {
+					return p.id === product.id ? { ...p, quantity: p.quantity + 1 } : p;
+				});
+			});
+		} else {
+			setProducts((prev) => {
+				return [...prev, product];
+			});
+		}
+	};
+};
 
 export const useCartContext = () => {
-    return useContext(cartContext);
-}
+	return useContext(cartContext);
+};
