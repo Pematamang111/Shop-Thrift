ac825c236916e8ce6848c96ae2c3b69f0174d922
Pema Sherpa <pematamang111@gmail.com>
2024-07-03 11:40:32 -0400

    create components

---
+++ client/src/components/productItem/ProductItem.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './productItem.css';
+
+export const ProductItem = () => {
+  return (
+    <div>ProductItem</div>
+  )
+}
---
+++ client/src/components/success/Success.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './success.css'
+
+export const Success = () => {
+  return (
+    <div>Success</div>
+  )
+}
