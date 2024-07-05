2076bc2780f788129dccc42d9c4d4225c24f0074
Pema Sherpa <pematamang111@gmail.com>
2024-07-05 19:39:56 -0400

    add cart provider around app

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { CartProvider } from './ctx/cartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<CartProvider>
+  <App />
+</CartProvider>
 );
 
