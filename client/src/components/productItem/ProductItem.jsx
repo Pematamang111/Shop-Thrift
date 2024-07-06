b233ff4070869ef91f4b25830c9ce709bc634e05
Pema Sherpa <pematamang111@gmail.com>
2024-07-05 20:07:03 -0400

    map through product in product jsx

--- client/src/components/productItem/ProductItem.jsx
+++ client/src/components/productItem/ProductItem.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './productItem.css';
+import products from '../../data/data'
+import { useCartContext } from '../../ctx/cartContext';
 
 export const Products = () => {
+  const { addProduct } = useCartContext();
   return (
     <div className='container'>
       {products.map((product) => {
         <div key={product.id}>
-
+            <img src={product.img} alt='apparal' />
+            <div className='product-details'>
+              <h3>{product.name}</h3>
+              <span>${product.price}</span>
+            </div>
+            <button onClick={()=> addProduct(product)}>Add To Cart</button>
         </div>
       }
 
