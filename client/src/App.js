ed9f02af990f15c35ac31a08ee501ac938819cf0
Pema Sherpa <pematamang111@gmail.com>
2024-07-09 19:09:28 -0400

    import routes and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div>
-      
+      <Routes>
+        <Route/>
+        <Route/>
+      </Routes>
     </div>
   );
 }
