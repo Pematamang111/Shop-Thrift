b90b549bfee0f2248653670c52d52bef93303fff
Pema Sherpa <pematamang111@gmail.com>
2024-07-13 20:56:53 -0400

    add cart icon in cart jsx

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import './cart.css';
+import { useCartContext } from '../../ctx/cartContext';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 export const Cart = () => {
-	return <div>Cart</div>;
+const {products, toggleCart, isOpen, removeProduct} = useCartContext
+
+	return (<div className='container'>
+		<div className="wrapper">
+			<div className='cartIcon' onClick={toggleCart}>
+               <AiOutlineShoppingCart size={25}/>
+			   <span className='cartNumber'>
+				{products?.length}
+			   </span>
+			</div>
+		</div>
+	</div>)
 };
