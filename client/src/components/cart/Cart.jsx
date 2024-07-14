25be1cb58e3852905279b06ecb82d75121616dc9
Pema Sherpa <pematamang111@gmail.com>
2024-07-13 21:15:56 -0400

    add isopen & if product > 0 in cart jsx

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -13,6 +13,22 @@ export const Cart = () => {
 					<AiOutlineShoppingCart size={25} />
 					<span className="cartNumber">{products?.length}</span>
 				</div>
+				{isOpen && (
+					<div className="cartContainer">
+						{products?.length > 0? (
+							<>
+							<h4>Products</h4>
+							<div className="productConatiner">
+								{products.map((product) => (
+                                  <div className="product" key={product.id}>
+									
+								  </div>
+								))}
+							</div>
+							</>
+						) : (<h3>No products in cart</h3>)}
+					</div>
+				)}
 			</div>
 		</div>
 	);
