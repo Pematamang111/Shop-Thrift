fa5b2bf6d2b3376109c825819c5a11a25c428376
Pema Sherpa <pematamang111@gmail.com>
2024-07-13 21:06:55 -0400

    format code in cart and css

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -4,16 +4,16 @@ import { useCartContext } from '../../ctx/cartContext';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 export const Cart = () => {
-const {products, toggleCart, isOpen, removeProduct} = useCartContext
+	const { products, toggleCart, isOpen, removeProduct } = useCartContext;
 
-	return (<div className='container'>
-		<div className="wrapper">
-			<div className='cartIcon' onClick={toggleCart}>
-               <AiOutlineShoppingCart size={25}/>
-			   <span className='cartNumber'>
-				{products?.length}
-			   </span>
+	return (
+		<div className="container">
+			<div className="wrapper">
+				<div className="cartIcon" onClick={toggleCart}>
+					<AiOutlineShoppingCart size={25} />
+					<span className="cartNumber">{products?.length}</span>
+				</div>
 			</div>
 		</div>
-	</div>)
+	);
 };
