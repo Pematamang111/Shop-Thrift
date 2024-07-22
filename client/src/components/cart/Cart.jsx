5d7eafa62df936c3961c69e64d4be4bf29dd0095
Pema Sherpa <pematamang111@gmail.com>
2024-07-22 18:58:50 -0400

    format code

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -15,27 +15,31 @@ export const Cart = () => {
 				</div>
 				{isOpen && (
 					<div className="cartContainer">
-						{products?.length > 0? (
+						{products?.length > 0 ? (
 							<>
-							<h4>Products</h4>
-							<div className="productConatiner">
-								{products.map((product) => (
-                                  <div className="product" key={product.id}>
-									<img src={product.img} alt="product" />
-									<div className="productDetails">
-										<h3>{product.name}</h3>
-										<span>{product.quantity} x ${product.price}</span>
-									</div>
-									<AiOutlineClose onClick={()=>removeProduct(product)}/>
-								  </div>
-								))}
-							</div>
-							<div className="controls">
-								<button onClick={() => {}}></button>
-								<span onClick={toggleCart}>Close Cart</span>
-							</div>
+								<h4>Products</h4>
+								<div className="productConatiner">
+									{products.map((product) => (
+										<div className="product" key={product.id}>
+											<img src={product.img} alt="product" />
+											<div className="productDetails">
+												<h3>{product.name}</h3>
+												<span>
+													{product.quantity} x ${product.price}
+												</span>
+											</div>
+											<AiOutlineClose onClick={() => removeProduct(product)} />
+										</div>
+									))}
+								</div>
+								<div className="controls">
+									<button onClick={() => {}}></button>
+									<span onClick={toggleCart}>Close Cart</span>
+								</div>
 							</>
-						) : (<h3>No products in cart</h3>)}
+						) : (
+							<h3>No products in cart</h3>
+						)}
 					</div>
 				)}
 			</div>
