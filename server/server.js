1e14dacf899b9e5ab143e1e475d2bf8f4b072fd4
Pema Sherpa <pematamang111@gmail.com>
2024-07-23 20:51:52 -0400

    listen app with process.env.PORT

--- server/server.js
+++ server/server.js
@@ -7,3 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.post()
+
+app.listen(process.env.PORT, () => console.log('Server is running successfully'))
