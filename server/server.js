2a843252dcde84f26a6f8119ad219cebe4fe76d7
Pema Sherpa <pematamang111@gmail.com>
2024-07-23 20:48:22 -0400

    use cors by app

--- server/server.js
+++ server/server.js
@@ -3,3 +3,5 @@ const dotenv = require('dotenv').config();
 const cors = require('cors');
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
 const app = express();
+
+app.use(cors());
