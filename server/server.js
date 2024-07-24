5d9a27dfab8aa59b964a31ac75a992b0b0a3c9b0
Pema Sherpa <pematamang111@gmail.com>
2024-07-23 20:47:32 -0400

    require express and asign with app

---
+++ server/server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
+const app = express();
